11de3e9ece576ccd5b985238a6066ecd1f5def06
Todd Schiller <todd.schiller@gmail.com>
2016-01-04 15:24:36 -0500

    Initial travis yml commit; local crx packaging

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        manifest: grunt.file.readJSON("src/manifest.json"),
         ts: {
             dist: {
                 tsconfig: true
@@ -8,17 +10,29 @@ module.exports = function(grunt) {
         copy: {
             dist: {
                 files: [
+                    {cwd: "src/", src: "manifest.json", dest: "dist/", expand: true},
                     {cwd: "src/css", src: ["**/*.css"], dest: "dist/css/", expand: true},
                     {cwd: "src/html", src: ["**/*.html"], dest: "dist/html/", expand: true},
-                    {cwd: "src/icons", src: ["**/*.png"], dest: "dist/icons/", expand: true},
-                    {src: "manifest.json", dest: "dist/"}
+                    {cwd: "src/icons", src: ["**/*.png"], dest: "dist/icons/", expand: true}
                 ]
+            }
+        },
+        crx: {
+            default: {
+                src: "dist/**/*",
+                dest: "crx/",
+                zipDest: "crx/",
+                options: {
+                    privateKey: process.env.KEY
+                }
             }
         }
     });
 
     grunt.loadNpmTasks("grunt-ts");
     grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-crx");
 
-    grunt.registerTask("dist", ["ts", "copy"]);
+    grunt.registerTask("build", ["ts", "copy"]);
+    grunt.registerTask("package", ["build", "crx"]);
 };
